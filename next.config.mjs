2b8c475f5a83a21e835707adef750d31836fd5b3
TanushaSingh3 <tanushasingh3@gmail.com>
2025-09-04 21:28:35 +0530

    CORS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // ✅ Match all files served from /public (any extension)
+          source: "/:path*",
+          headers: [
+            {
+              key: "Access-Control-Allow-Origin",
+              value: "https://report.sand-box.info" // or "*" if you want fully public
+            },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, OPTIONS"
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value: "Content-Type"
+            }
+          ]
+        }
+      ];
+    }
+  };
+  
+ // module.exports = nextConfig;
+  
 
 export default nextConfig;
+
+
